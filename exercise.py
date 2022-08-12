25a85e94e7e8c734ca453eea7cae8875a15d69aa
c-shantanu <itsmeshantranuchauhan@gmail.com>
2022-08-12 09:31:39 +0200

    Ex4

---
+++ exercise.py
@@ -0,0 +1,35 @@
+# do exercise here
+# upload to github for portfolio
+# thank me later when your git graph is as green as the python money u gonna earn
+
+# Task 1 - comparison operators
+
+for i in range(3):
+    a = int(input("Enter First Number: "))
+    b = int(input("Enter Second Number: "))
+    if b > a:
+        print("b is greater than a")
+    elif a == b:
+        print("a and b are equal")
+    elif a > b and b < a:
+        print("Both conditions are True")
+    if a > b or b > a:
+        print("At least one of the conditions is True")
+
+i=0
+while i < 3:
+    a = int(input("Enter First Number: "))
+    b = int(input("Enter Second Number: "))
+    if b > a:
+        print("b is greater than a")
+    else:
+        a == b
+        print("a and b are equal")
+    if a > b and b < a:
+        print("Both conditions are True")
+    else:
+            print("At least one of the conditions is True")
+    i += 1
+
+
+bin(411)
